14a851e277ec4a0fcb429b1526e78e0635170346
Ivan-Krul <kiwiivanya128@gmail.com>
2024-09-15 15:15:57 +0200

    scrapped data from ScriptPostum's OC and made character.js final

--- js/character.js
+++ js/character.js
@@ -1,12 +1,7 @@
 
 // don't touch >:3
 async function fetchFile(filePath) {
-  return fetch("../"+filePath)
-    .then(response => response.text())
-    .catch(error => {
-      console.error('*Error fetching ' + filePath + ':', error);
-      throw '*Error fetching ' + filePath + ':', error;
-    });
+  return fetch("../"+filePath);
 }
 
 function getURLParams() {
@@ -20,10 +15,43 @@ async function main() {
   let oc = getURLParams().get("oc");
   document.getElementById("about_who").innerText = oc.toUpperCase();
   
-  await delay(1000);
+  await delay(2000);
   
-  let json = await fetchFile("characters/"+oc+".json")
+  document.getElementById("profile").style.display = "inherit";
   
+  let is_invalid = false;
+  
+  const json = await fetchFile("characters/"+oc+".json")
+    .then(resp => resp.json())
+    .catch((error) => {
+      console.error(error);
+      document.getElementById("error_string").innerText = error;
+      is_invalid = true;
+  });
+  
+  if(is_invalid) return;
+  
+  document.getElementById("actual_profile").style.display = "inherit";
+  
+  document.getElementById("name").innerText =     json.NAME;
+  document.getElementById("pronouns").innerText = json.PRONOUNS;
+  document.getElementById("age").innerText =      json.AGE === null ? "NULL" : json.AGE;
+  document.getElementById("likes").innerText =    json.LIKES;
+  document.getElementById("dislikes").innerText = json.DISLIKES;
+  
+  document.getElementById("pfp").src = "../"+json.PFP_PATH;
+  
+  const bio = await fetchFile("characters/"+oc+".bio")
+    .then(resp => resp.text())
+    .catch((error) => {
+      console.error(error);
+      document.getElementById("error_string").innerText = error;
+      is_invalid = true;
+  });
+  
+  const new_bio = bio.replaceAll("\n", " ").replaceAll("\r", "").replaceAll("\\n", "\n");
+  
+  document.getElementById("bio").innerText = is_invalid ? "NULL" : new_bio;
   
 }
 
