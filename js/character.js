71d2151c7bf06d9b72ec528f476272ea343857db
Ivan-Krul <kiwiivanya128@gmail.com>
2024-12-07 21:13:42 +0100

    added version for refs and made version 2 of render

--- js/character.js
+++ js/character.js
@@ -9,6 +9,33 @@ function getURLParams() {
   return new URLSearchParams(window.location.search);
 }
 
+function outputJSONCompatable(json) {
+  switch(json.REF_VERSION) {
+    case 1: outputRefV1(json); break;
+    case 2: outputRefV2(json); break;
+  }
+}
+
+function outputRefV1(json) {
+  document.getElementById("name").innerText =     json.NAME;
+  document.getElementById("pronouns").innerText = json.PRONOUNS;
+  document.getElementById("age").innerText =      json.AGE === null ? "NULL" : json.AGE;
+  document.getElementById("likes").innerText =    json.LIKES;
+  document.getElementById("dislikes").innerText = json.DISLIKES;
+  
+  document.getElementById("pfp").src = "../"+json.PFP_PATH;
+}
+
+function outputRefV2(json) {
+  outputRefV1(json);
+  
+  document.getElementById("prop_rv_2").style.display = "inherit";
+  
+  document.getElementById("sexuality").innerText = json.SEXUALITY;
+  document.getElementById("work").innerText =      json.WORK;
+  document.getElementById("notes").innerText =     json.NOTES;
+}
+
 async function main() {
   let oc = getURLParams().get("oc");
   document.getElementById("about_who").innerText = oc.toUpperCase();
@@ -31,15 +58,9 @@ async function main() {
   
   document.getElementById("actual_profile").style.display = "inherit";
   
-  document.getElementById("name").innerText =     json.NAME;
-  document.getElementById("pronouns").innerText = json.PRONOUNS;
-  document.getElementById("age").innerText =      json.AGE === null ? "NULL" : json.AGE;
-  document.getElementById("likes").innerText =    json.LIKES;
-  document.getElementById("dislikes").innerText = json.DISLIKES;
+  outputJSONCompatable(json);
   
-  document.getElementById("pfp").src = "../"+json.PFP_PATH;
-  
-  const bio = await fetchFile("characters/"+oc+".bio")
+  const bio = await fetchFile(`characters/${oc}.bio`)
     .then(resp => resp.text())
     .catch((error) => {
       console.error(error);
